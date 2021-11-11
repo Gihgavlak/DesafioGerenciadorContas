f1cf3d6e14a3742e45d622b6baa83cc79df7bf3a
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 20:10:38 -0300

    Criar Classe ConversorModel

---
+++ src/main/java/br/com/zup/GerenciadorContas/componente/ConversorModel.java
@@ -0,0 +1,12 @@
+package br.com.zup.GerenciadorContas.componente;
+
+import org.modelmapper.ModelMapper;
+
+public class ConversorModel {
+
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+
+    }
+
+}
