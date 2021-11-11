38cff9fa5762b686a3b4120a3bef0dff2c3d00aa
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 10:42:15 -0300

    Criar Classe de entrada dto implementando os getter e setter e construtor vazio

--- src/main/java/br/com/zup/GerenciadorContas/GerenciadorDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/GerenciadorDTO.java
@@ -1,2 +1,45 @@
-package br.com.zup.GerenciadorContas;public class GerenciadorDTO {
+package br.com.zup.GerenciadorContas;
+
+public class GerenciadorDTO {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+
+    public GerenciadorDTO() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
 }
