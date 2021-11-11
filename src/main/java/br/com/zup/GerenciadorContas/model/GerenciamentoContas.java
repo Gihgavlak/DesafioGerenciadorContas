5d87f3251ae8909033afba9fd543a217bab3392d
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 15:02:48 -0300

    Criar Classe model

--- src/main/java/br/com/zup/GerenciadorContas/model/GerenciamentoContas.java
+++ src/main/java/br/com/zup/GerenciadorContas/model/GerenciamentoContas.java
@@ -1,2 +1,90 @@
-package br.com.zup.GerenciadorContas.model;public class GerenciamentoContas {
+package br.com.zup.GerenciadorContas.model;
+
+import br.com.zup.GerenciadorContas.enun.Status;
+import br.com.zup.GerenciadorContas.enun.Tipo;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contas")
+public class GerenciamentoContas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private double valor;
+    @Column(nullable = false)
+    private Tipo tipo;
+    @Column(nullable = false)
+    private LocalDate dataDeVencimento;
+    @Column(nullable = true)
+    private LocalDateTime dataDePagamento;
+    @Column(nullable = false)
+    private Status status;
+
+    public GerenciamentoContas() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
 }
