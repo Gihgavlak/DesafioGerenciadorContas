c2bf7a94dc1716967300a1c228d907d1aba19452
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 13:34:38 -0300

    Criar Classe de saida DTO com construtor vazio e com getter e setter

---
+++ src/main/java/br/com/zup/GerenciadorContas/dto/SaidaContaDTO.java
@@ -0,0 +1,75 @@
+package br.com.zup.GerenciadorContas.dto;
+
+import br.com.zup.GerenciadorContas.enun.Status;
+import br.com.zup.GerenciadorContas.enun.Tipo;
+
+public class SaidaContaDTO {
+    private int id;
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Status status;
+
+    public SaidaContaDTO() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+}
