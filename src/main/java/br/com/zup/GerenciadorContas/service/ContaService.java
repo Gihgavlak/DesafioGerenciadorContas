ec44ea3f66a924cd3f1699cf82cd28096e63328e
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 22:42:49 -0300

    Criar método para cadastrar conta

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,8 +1,12 @@
 package br.com.zup.GerenciadorContas.service;
 
+import br.com.zup.GerenciadorContas.enun.Status;
+import br.com.zup.GerenciadorContas.model.GerenciamentoContas;
 import br.com.zup.GerenciadorContas.repository.GerenciamentoContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
@@ -10,6 +14,14 @@ public class ContaService {
     @Autowired
     private GerenciamentoContaRepository gerenciamentoContaRepository;
 
+    public GerenciamentoContas cadastrarConta(GerenciamentoContas gerenciamentoContas){
+        if (gerenciamentoContas.getDataDeVencimento().isBefore(LocalDate.now())){
+            gerenciamentoContas.setStatus(Status.VENCIDA);
+        }else{
+            gerenciamentoContas.setStatus(Status.AGUARDANDO);
+        }
+        return gerenciamentoContaRepository.save(gerenciamentoContas);
+    }
 
 
 }
