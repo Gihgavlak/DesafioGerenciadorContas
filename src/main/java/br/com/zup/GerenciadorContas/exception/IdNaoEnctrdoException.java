e50bdbf38a4a5190a99a69bc1c8d5a4ffa91dd9d
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-12 02:45:14 -0300

    Criar Classe para Tratar exceção de ID

---
+++ src/main/java/br/com/zup/GerenciadorContas/exception/IdNaoEnctrdoException.java
@@ -0,0 +1,8 @@
+package br.com.zup.GerenciadorContas.exception;
+
+public class IdNaoEnctrdoException extends RuntimeException{
+
+    public IdNaoEnctrdoException (String mensagem){
+        super(mensagem);
+    }
+}
