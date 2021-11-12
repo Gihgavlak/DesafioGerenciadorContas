9a861891477f02dd8f460754d7335f4612ae8632
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-12 11:52:51 -0300

    Criar classe de mensagem para erro de exceção

---
+++ src/main/java/br/com/zup/GerenciadorContas/exception/MensagemExcecao.java
@@ -0,0 +1,19 @@
+package br.com.zup.GerenciadorContas.exception;
+
+public class MensagemExcecao {
+
+    private String mensagem;
+
+    public MensagemExcecao() {
+
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+}
