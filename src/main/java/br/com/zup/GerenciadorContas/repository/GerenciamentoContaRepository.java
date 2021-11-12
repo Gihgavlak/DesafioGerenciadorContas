53c737cd35b0b38d2a224656961bfeabb6e4b73d
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-12 14:19:27 -0300

    Criar lista para filtro

--- src/main/java/br/com/zup/GerenciadorContas/repository/GerenciamentoContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorContas/repository/GerenciamentoContaRepository.java
@@ -1,8 +1,16 @@
 package br.com.zup.GerenciadorContas.repository;
 
+import br.com.zup.GerenciadorContas.enun.Status;
+import br.com.zup.GerenciadorContas.enun.Tipo;
 import br.com.zup.GerenciadorContas.model.GerenciamentoContas;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GerenciamentoContaRepository extends CrudRepository<GerenciamentoContas, Integer> {
+    List<GerenciamentoContas> findAllByStatus(Status status);
+
+    List<GerenciamentoContas> findAllByTipo(Tipo tipo);
+
 
 }
