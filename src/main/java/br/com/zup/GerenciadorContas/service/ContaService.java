360aa216fb86175acbd0a46be6dab7ee01da470f
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 21:44:49 -0300

    Fazer a injeção da Repository

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,8 +1,15 @@
 package br.com.zup.GerenciadorContas.service;
 
+import br.com.zup.GerenciadorContas.repository.GerenciamentoContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaService {
+
+    @Autowired
+    private GerenciamentoContaRepository gerenciamentoContaRepository;
+
+
 
 }
