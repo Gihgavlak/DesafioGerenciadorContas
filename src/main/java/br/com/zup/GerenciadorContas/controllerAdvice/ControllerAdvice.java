23efd3f7d37fda60f44d01f26207883b11269f58
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-12 14:23:47 -0300

    Criar Classe ControllerAdvice

--- src/main/java/br/com/zup/GerenciadorContas/controllerAdvice/ControllerAdvice.java
+++ src/main/java/br/com/zup/GerenciadorContas/controllerAdvice/ControllerAdvice.java
@@ -1,2 +1,29 @@
-package br.com.zup.GerenciadorContas.controllerAdvice;public class ControllerAdvice {
+package br.com.zup.GerenciadorContas.controllerAdvice;
+
+import br.com.zup.GerenciadorContas.exception.MensagemExcecao;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemExcecao> tratarErrosDeValidacao (MethodArgumentNotValidException excecao){
+        List<MensagemExcecao> erros = new ArrayList<>();
+
+        for (FieldError referencia : excecao.getFieldErrors()){
+            MensagemExcecao mensagemDeErro = new MensagemExcecao(referencia.getDefaultMessage());
+            erros.add(mensagemDeErro);
+        }
+
+        return erros;
+    }
+
 }
