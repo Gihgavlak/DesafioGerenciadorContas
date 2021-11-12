1782201e79269e392da13e4dd70a8658bb2453aa
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-12 00:21:52 -0300

    Inserir Anotação de Component e Bean

--- src/main/java/br/com/zup/GerenciadorContas/componente/ConversorModel.java
+++ src/main/java/br/com/zup/GerenciadorContas/componente/ConversorModel.java
@@ -1,9 +1,13 @@
 package br.com.zup.GerenciadorContas.componente;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConversorModel {
 
+    @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
 
