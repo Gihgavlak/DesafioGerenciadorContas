713299441f759df952712e9980be3cd56400fd01
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-11 22:22:24 -0300

    Injetar Service como dependencia

--- src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
@@ -1,10 +1,15 @@
 package br.com.zup.GerenciadorContas.controller;
 
+import br.com.zup.GerenciadorContas.service.ContaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
+
+    @Autowired
+    ContaService contaService;
 
 }
